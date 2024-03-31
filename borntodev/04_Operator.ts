af761ad26f3ce997b5b6ca6e91fac4799de34e80
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-31 23:42:00 +0700

    add: operators

---
+++ borntodev/04_Operator.ts
@@ -0,0 +1,12 @@
+let x:number = 5 - 4;
+let y:number = 5 + 4;
+let c:number = x += y;
+
+y++
+console.log(x);
+console.log(y);
+console.log(c);
+
+console.log(10 == y);
+
+console.log(true && false);
